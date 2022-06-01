8719fc2ae6b2071caf1a8ff550198caf5729f01b
stefvtls <stefvtls@gmail.com>
2022-06-01 15:10:53 +0200

    improved timer mechanizm. got rid of timer bugs

--- main.py
+++ main.py
@@ -25,13 +25,14 @@ LENGTH = len(frequency_dictionary)
 
 # button mechanism
 def choose_random_word():
-    global ind
+    global ind, flip_timer
+    window.after_cancel(flip_timer)
     index = random.randint(0, LENGTH)
     card.itemconfig(card_background, image=front_card)
     card.itemconfig(text_top, fill="black", text=LANGUAGE_TO_LEARN)
     card.itemconfig(text_bottom, fill="black", text=frequency_dictionary[index][LANGUAGE_TO_LEARN])
     ind = index
-    window.after(ms=TIMER, func=flip)
+    flip_timer = window.after(ms=TIMER, func=flip)
 
 
 # generating translation card. stopping the timer after the flip
@@ -39,7 +40,7 @@ def flip():
     card.itemconfig(card_background, image=back_card)
     card.itemconfig(text_top, fill="white", text=LANGUAGE_NATIVE)
     card.itemconfig(text_bottom, fill="white", text=frequency_dictionary[ind][LANGUAGE_NATIVE])
-    window.after_cancel()
+
 
 
 # APP LAYOUT - creating visual user interface
@@ -48,6 +49,8 @@ window = tkinter.Tk()
 window.minsize(width=900, height=626)
 window.title("Learn Dutch fast - 5000 most common used words in movie subtitles")
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+# timer
+flip_timer = window.after(ms=TIMER, func=flip)
 # photo images
 front_card = tkinter.PhotoImage(file="images/card_front.png")
 back_card = tkinter.PhotoImage(file="images/card_back.png")
@@ -59,6 +62,8 @@ card_background = card.create_image(0, 0, anchor="nw", image=front_card)
 text_top = card.create_text(X_TXT_COORDINATES, Y_TXT_COORDINATES_TOP, text=LANGUAGE_TO_LEARN, font=FONT_TOP)
 text_bottom = card.create_text(X_TXT_COORDINATES, Y_TXT_COORDINATES_BOTTOM, text="are you ready?", font=FONT_BOTTOM)
 card.grid(row=0, column=0, columnspan=2)
+
+
 # buttons
 wrong_button = tkinter.Button(image=wrong_image, highlightthickness=0, command=choose_random_word)
 right_button = tkinter.Button(image=right_image, highlightthickness=0, command=choose_random_word)
@@ -69,8 +74,7 @@ right_button.grid(column=1, row=1)
 # loading first flash card at the beginning of the program
 choose_random_word()
 
-# timer
-window.after(ms=TIMER, func=flip)
+
 
 # END
 window.mainloop()
